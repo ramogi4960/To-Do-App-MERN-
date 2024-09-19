8c410e72dc5bb9264c2446a406ac013bc0039d2a
Kevin <kramzyvine@icloud.com>
2024-09-19 21:25:07 +0300

    added simple basic CSS styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <main id='main-app'>
+      <div id="top-section">
+        <form id="item-form">
+          <input type='text' id='new-item' name='new-item' placeholder='Enter new item...' />
+          <button id='submit-button' type='submit'><i className="fa-solid fa-paper-plane submit"></i></button>
+        </form>
+      </div>
+      <div id="bottom-section">
+        <div className="item">
+          <p id='item-description'>Wash the car</p>
+          <div className='buttons'>
+            <button className='edit-button'><i className="fa-solid fa-pen-to-square edit"></i></button>
+            <button className='delete-button'><i className="fa-solid fa-trash delete"></i></button>
+          </div>
+        </div>
+        <div className="item">
+          <p id='item-description'>Finish assignments</p>
+          <div className='buttons'>
+            <button className='edit-button'><i className="fa-solid fa-pen-to-square edit"></i></button>
+            <button className='delete-button'><i className="fa-solid fa-trash delete"></i></button>
+          </div>
+        </div>
 
-export default App;
+        <div className="item">
+          <p id='item-description'>Do some React Native project</p>
+          <div className='buttons'>
+            <button className='edit-button'><i className="fa-solid fa-pen-to-square edit"></i></button>
+            <button className='delete-button'><i className="fa-solid fa-trash delete"></i></button>
+          </div>
+        </div>
+
+        <div className="item">
+          <p id='item-description'>Complete the leetcode daily problem</p>
+          <div className='buttons'>
+            <button className='edit-button'><i className="fa-solid fa-pen-to-square edit"></i></button>
+            <button className='delete-button'><i className="fa-solid fa-trash delete"></i></button>
+          </div>
+        </div>
+
+        <div className="item">
+          <p id='item-description'>Do homework with Remy</p>
+          <div className='buttons'>
+            <button className='edit-button'><i className="fa-solid fa-pen-to-square edit"></i></button>
+            <button className='delete-button'><i className="fa-solid fa-trash delete"></i></button>
+          </div>
+        </div>
+
+        <div className="item">
+          <p id='item-description'>Get some Jabba</p>
+          <div className='buttons'>
+            <button className='edit-button'><i className="fa-solid fa-pen-to-square edit"></i></button>
+            <button className='delete-button'><i className="fa-solid fa-trash delete"></i></button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
